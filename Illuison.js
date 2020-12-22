f446c96399a8b3d01804db48c0eca7e2e21e8129
naniJwalk <71357663+naniJwalk@users.noreply.github.com>
2020-12-21 23:42:21 -0500

    Add files via upload

---
+++ Illuison.js
@@ -0,0 +1,18 @@
+class illuison{
+    constructor(x,y){
+        this.body=Bodies.rectangle(x,y,20,100);
+        this.width=20;
+        this.height=100;
+        this.image = loadImage("skeleton.jpg");
+        World.add(world,this.body);
+    }
+    updateY(){     
+            Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+        
+    }
+    showIlluison(){
+        fill("blue")
+        imageMode(CENTER);
+        image(this.body.position.x,this.body.position.y,20,100);
+    }
+}
